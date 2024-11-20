26d1a2090d023905b4b252c2979e01f1d8641349
yang <yyang48699997@gmail.com>
2024-11-20 22:28:17 +0800

    update encryption

--- server/encryption.py
+++ server/encryption.py
@@ -1,10 +1,12 @@
-from passlib.hash import pbkdf2_sha256
+import hashlib
 
 
-def hash_password(password):
-    return pbkdf2_sha256.hash(password)
+def hash_password_fixed(password, salt="yu_yang_hao_shu_ai"):
+    combined = password + salt
+    hash_object = hashlib.sha256(combined.encode('utf-8'))
+    return hash_object.hexdigest()
 
 
 def check_password(password, hashed_password):
-    return pbkdf2_sha256.verify(password, hashed_password)
+    return hash_password_fixed(password) == hash_password_fixed(hashed_password)
 
